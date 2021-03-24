780e19546adc53b4443711f20901daa48b194cff
the_new_sky <loris@xtns.dev>
2021-03-24 15:18:14 +0100

    Search
    Co-authored-by: Joris MARTIN <JorisMARTIN@users.noreply.github.com>

--- api.js
+++ api.js
@@ -21,7 +21,6 @@ function convertToItem(openfoodfacts) {
 async function searchProductsByName(query) {
     const request = await fetch(`https://fr.openfoodfacts.org/cgi/search.pl?search_terms=${encodeURIComponent(query)}&search_simple=1&action=process&json=1`)
     const json = await request.json();
-    console.log(json);
 
     return json.products.map((product) => convertToItem(product))
 }
@@ -37,12 +36,74 @@ async function searchProductByBarcode(barcode) {
     return convertToItem(json.product);
 }
 
-function computeScore(items) {
+function computeNutriScore(items) {
     const nutriscores = items
         .filter((item) => item.openfoodfacts.nutriscore_grade != null)
-        .map((item) => 'edcba'.indexOf(item.openfoodfacts.nutriscore_grade) * 2);
+        .map((item) => 'edcba'.indexOf(item.openfoodfacts.nutriscore_grade) * 4);
 
     return Math.round(nutriscores.reduce((a, b) => a + b, 0) / (nutriscores.length + 1));
 }
 
-// todo : getAdvice(items)
+function computeNovaScore(items) {
+    const novascores = items
+        .filter((item) => item.openfoodfacts.nova_groups != null)
+        .map((item) => 10 - (parseInt(item.openfoodfacts.nova_groups) * 2.5));
+
+    if (novascores.length == 0) {
+        return 10;
+    }
+
+    return Math.round(novascores.reduce((a, b) => a + b, 0) / (novascores.length + 1));
+}
+
+function computeEcoScore(items) {
+    const ecoscores = items
+        .filter((item) => item.openfoodfacts.ecoscore_score != null)
+        .map((item) => item.openfoodfacts.ecoscore_score / 10);
+
+    return Math.round(ecoscores.reduce((a, b) => a + b, 0) / (ecoscores.length + 1));
+}
+
+function computeAverageAdditives(items) {
+    const additives = items
+        .filter((item) => item.openfoodfacts.additives_n != null)
+        .map((item) => item.openfoodfacts.additives_n);
+
+    return Math.round(additives.reduce((a, b) => a + b, 0) / (additives.length + 1));
+}
+
+function computeScore(items) {
+    const nutriscore = computeNutriScore(items);
+    const novascore = computeNovaScore(items);
+    const additives = Math.max(20 - computeAverageAdditives(items), 0) / 2;
+
+    if (items.length == 0) {
+        return 0;
+    }
+
+    return Math.round(nutriscore * 0.8 + novascore * 0.1 + additives * 0.1);
+}
+
+function computeIndividualScore(item) {
+    console.log(item.name, computeNutriScore([item]), computeNovaScore([item]))
+    return computeScore([item]);
+}
+
+function getAdvice(items) {
+    const nutriscore = computeNutriScore(items);
+    const novascore = computeNovaScore(items);
+    const ecoscore = computeEcoScore(items);
+    const additives = computeAverageAdditives(items);
+
+    if (items.length === 0) {
+        return '';
+    } else if (nutriscore < 5) {
+        return "Réduisez votre consommation d'aliments à forte teneur en sucre, graisses saturées ou en sel.";
+    } else if (novascore < 4) {
+        return "Privilégiez des produits minimalement transformés.";
+    } else if (ecoscore < 5) {
+        return "Préférez des produits à l'impact environnemental plus faible.";
+    } else {
+        return `En moyenne, chaque produit de votre liste contient ${additives} additifs.`;
+    }
+}
--- main.js
+++ main.js
@@ -4,7 +4,10 @@ const app = new Vue({
     el: '#app',
     data() {
         return {
-            items: []
+            items: [],
+            addMenuOpened: false,
+            lastSearch: "",
+            searchAnswer: []
         };
     },
 
@@ -16,7 +19,9 @@ const app = new Vue({
         healthMessage() {
             const score = this.score;
 
-            if (score < 3) {
+            if (this.items.length === 0) {
+                return '-';
+            } else if (score < 3) {
                 return 'Mauvais';
             } else if (score  < 6) {
                 return 'Moyen';
@@ -26,9 +31,11 @@ const app = new Vue({
         },
 
         nutriscore() {
-            const score = this.score;
+            const score = computeNutriScore(this.items);
 
-            if (score <= 2) {
+            if (score === 0) {
+                return '-';
+            } else if (score <= 2) {
                 return 'E';
             } else if (score  <= 4) {
                 return 'D';
@@ -39,19 +46,53 @@ const app = new Vue({
             } else {
                 return 'A';
             }
-        }
+        },
 
-        // todo: advice()
+        advice() {
+            return getAdvice(this.items);
+        },
+
+        color() {
+            const score = this.score;
+
+            if (this.items.length == 0) {
+                return 'circle-grey';
+            } else if (score < 3) {
+                return 'circle-red';
+            } else if (score  < 6) {
+                return 'circle-yellow';
+            } else {
+                return 'circle-green';
+            }
+        },
+
+        healthyItems() {
+            return this.items.filter((item) => computeIndividualScore(item) >= 6);
+        },
+
+        middleItems() {
+            return this.items.filter((item) => {
+                const score = computeIndividualScore(item);
+                return score < 6 && score > 3;
+            });
+        },
+
+        badItems() {
+            return this.items.filter((item) => computeIndividualScore(item) <= 3);
+        },
     },
 
     methods: {
-        search: async () => {
-            await searchProductsByName('oréo');
-        }
-    },
+        async searchProduct(event) {
+            event.preventDefault();
 
-    created() {
-        searchProductsByName('soupe')
-            .then((items) => this.items = items);
+            const result = await searchProductsByName(this.lastSearch);
+            this.searchAnswer = result.slice(0, 20);
+        },
+        
+        addToList(item){
+            this.items.push(item);
+            this.addMenuOpened = false;
+        }
     }
 });
